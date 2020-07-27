496c87be083a2ae4fee7adc48debbcbdd48e8554
Mridul <mridultpcs@gmail.com>
2020-07-27 16:28:33 +0530

    Theme and form setup

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,11 +8,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +25,4 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseService } from '../core/Services/base.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private base: BaseService, private fb: FormBuilder,
+    private nav: NavController) {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() { }
+
+  attemptLogin() {
+    // if (this.loginForm.valid) {
+    //   try {
+    //     this.base.login(this.loginForm.value).subscribe(response => {
+    //       if (response.status === "success") {
+    //         alert('login success');
+    //         this.nav.navigateRoot('/home');
+    //       }
+    //       else if (response.status === "error") {
+    //         alert(response.txt);
+    //       }
+    //     });
+    //   } catch (error) {
+    //     alert('Something went wrong');
+    //   }
+    // }
+    // else{
+    //   alert('Please enter valid information');
+    // }
+    this.nav.navigateRoot('/home');
+  }
 
 }
