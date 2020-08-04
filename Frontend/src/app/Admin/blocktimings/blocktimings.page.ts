18fb23022bc68ab27ba8ede0273915fffd79e31a
Mridul <mridultpcs@gmail.com>
2020-08-04 18:58:40 +0530

    block level work

--- Frontend/src/app/Admin/blocktimings/blocktimings.page.ts
+++ Frontend/src/app/Admin/blocktimings/blocktimings.page.ts
@@ -13,10 +13,11 @@ export class BlocktimingsPage implements OnInit {
   blockId: number;
   valveDetails: object;
   valveTime: number;
+  startTime: string;
+  endTime: string;
 
   constructor(private activatedRoute: ActivatedRoute, private farm: FarmService,
     private alertCtrl: AlertController, private nav: NavController) {
-    this.valveTime = 1;
   }
 
   ngOnInit() {
@@ -32,6 +33,8 @@ export class BlocktimingsPage implements OnInit {
         if (response.status === "success") {
           this.valveDetails = response.data ? response.data[0] : null;
           this.valveTime = this.valveDetails ? parseInt(this.valveDetails['instruction']) : 1;
+          this.startTime = this.valveDetails && this.valveDetails['instruction_start_time'] ? this.getDateT(this.valveDetails['instruction_start_time']) : '';
+          this.endTime = this.valveDetails && this.valveDetails['instruction_end_time'] ? this.getDateT(this.valveDetails['instruction_end_time']) : '';
         }
       });
     } catch (error) {
@@ -39,29 +42,51 @@ export class BlocktimingsPage implements OnInit {
     }
   }
 
-  assignTime(valveTime) {
-    if (valveTime) {
-      try {
-        this.farm.assignHours({
-          block: this.blockId,
-          time: valveTime
-        }).subscribe(response => {
-          if (response.status === "success") {
-            this.getBlockValveDetails();
-            alert('Time added');
-          }
-        })
-      } catch (error) {
-        alert('something went wrong');
+  getDateT(time: string): string {
+    let d = time.split(":").map(e => +e);
+    let b = new Date();
+    return new Date(b.getFullYear(), b.getMonth(), b.getDate(), d[0], d[1]).toISOString();
+  }
+
+  assignTime(valveTime, st, end) {
+    if (valveTime && st && end) {
+      if (this.diff_hours(new Date(st), new Date(end)) === valveTime) {
+        try {
+          this.farm.assignHours({
+            block: this.blockId,
+            time: valveTime,
+            start_time: `${new Date(st).getHours()}:${new Date(st).getMinutes()}`,
+            end_time: `${new Date(end).getHours()}:${new Date(end).getMinutes()}`
+          }).subscribe(response => {
+            if (response.status === "success") {
+              this.getBlockValveDetails();
+              alert('Time added');
+            }
+          })
+        } catch (error) {
+          alert('something went wrong');
+        }
       }
+      else {
+        alert('Start & end time difference is more/less than valve hours');
+      }
+
+    } else {
+      alert('Please fill all the details');
     }
   }
 
-  async presentAlertConfirm(valveTime) {
+  diff_hours(dt2, dt1) {
+    var res = Math.abs(dt1 - dt2) / 1000;
+    var hours = Math.floor(res / 3600) % 24;
+    return hours;
+  }
+
+  async presentAlertConfirm(valveTime, st, end) {
     const alert = await this.alertCtrl.create({
       cssClass: 'my-custom-class',
       header: 'Confirm',
-      message: 'Are you sure you want to update this valve time?',
+      message: 'Are you sure you want to update this valve information?',
       buttons: [
         {
           text: 'No',
@@ -73,7 +98,7 @@ export class BlocktimingsPage implements OnInit {
         }, {
           text: 'Yes',
           handler: () => {
-            this.updateTime(valveTime)
+            this.updateTime(valveTime, st, end)
           }
         }
       ]
@@ -81,26 +106,134 @@ export class BlocktimingsPage implements OnInit {
     await alert.present();
   }
 
-  updateTime(valveTime) {
-    if (valveTime) {
-      try {
-        this.farm.updateHours({
-          id: this.valveDetails['id'],
-          time: valveTime
-        }).subscribe(response => {
-          if (response.status === "success") {
-            this.getBlockValveDetails();
-            alert('Time updated');
-          }
-        })
-      } catch (error) {
-        alert('something went wrong');
+  updateTime(valveTime, st, end) {
+    if (valveTime && st && end) {
+      if (this.diff_hours(new Date(st), new Date(end)) === valveTime) {
+        try {
+          this.farm.updateHours({
+            id: this.valveDetails['id'],
+            time: valveTime,
+            start_time: `${new Date(st).getHours()}:${new Date(st).getMinutes()}`,
+            end_time: `${new Date(end).getHours()}:${new Date(end).getMinutes()}`
+          }).subscribe(response => {
+            if (response.status === "success") {
+              this.getBlockValveDetails();
+              alert('Time updated');
+            }
+          });
+        } catch (error) {
+          alert('something went wrong');
+        }
       }
+      else {
+        alert('Start & end time difference is more/less than valve hours');
+      }
+    }
+    else {
+      alert('Please fill all the details');
     }
   }
 
-  test12(a,b){
-    console.log(a + ' ' + b);
+  startValve() {
+    try {
+      this.farm.startValve({
+        id: this.valveDetails['id'],
+        start_time: `${new Date().getHours()}:${new Date().getMinutes()}`
+      }).subscribe(response => {
+        if (response.status === "success") {
+          this.getBlockValveDetails();
+          alert('Valve started');
+        }
+      });
+    } catch (error) {
+      alert('something went wrong');
+    }
+  }
+
+  stopValve() {
+    try {
+      this.farm.stopValve({
+        id: this.valveDetails['id'],
+        stop_time: `${new Date().getHours()}:${new Date().getMinutes()}`
+      }).subscribe(response => {
+        if (response.status === "success") {
+          this.getBlockValveDetails();
+          alert('Valve stopped');
+        }
+      });
+    } catch (error) {
+      alert('something went wrong');
+    }
+  }
+
+  async confirmReport() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm',
+      message: 'Please mention the issue for interruption caused.',
+      inputs: [
+        {
+          name: 'reason',
+          type: 'text',
+          placeholder: 'Interruption reason',
+        }
+      ],
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            // console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Yes',
+          handler: (data) => {
+            this.reportValveIssue(data.reason);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  reportValveIssue(reason: string) {
+    if (reason.trim() != "") {
+      try {
+        this.farm.reportIssue({
+          id: this.valveDetails['id'],
+          time: `${new Date().getHours()}:${new Date().getMinutes()}`,
+          interruption_reason: reason
+        }).subscribe(response => {
+          if (response.status === "success") {
+            this.getBlockValveDetails();
+            alert('Valve issue reported');
+          }
+        });
+      } catch (error) {
+        alert('something went wrong');
+      }
+    } else {
+      alert('Reason can not be empty');
+      this.confirmReport();
+    }
+
+  }
+
+  resumeValve() {
+    try {
+      this.farm.resumeValve({
+        id: this.valveDetails['id'],
+        start_time: `${new Date().getHours()}:${new Date().getMinutes()}`
+      }).subscribe(response => {
+        if (response.status === "success") {
+          this.getBlockValveDetails();
+          alert('Valve resumed');
+        }
+      });
+    } catch (error) {
+      alert('something went wrong');
+    }
   }
 
 }
