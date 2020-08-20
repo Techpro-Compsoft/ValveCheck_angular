3f383aa336da95d08148b5f2430b326ab446ab13
rajaman821 <50168576+amanTP@users.noreply.github.com>
2020-08-20 10:25:10 +0530

    routing changes and operator profile removed

--- Frontend/src/app/Operator/operator-dashboard/operator-dashboard.page.ts
+++ Frontend/src/app/Operator/operator-dashboard/operator-dashboard.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
 import { OperatorService } from 'src/app/core/Services/Operator/operator.service';
 import { BaseService } from 'src/app/core/Services/base.service';
 
@@ -10,13 +10,21 @@ import { BaseService } from 'src/app/core/Services/base.service';
 })
 export class OperatorDashboardPage implements OnInit {
   farmsList: Array<object>;
+  operatorId: any;
 
   constructor(public navCtrl: NavController,
     public operatorService: OperatorService,
-    public base: BaseService) { }
+    public base: BaseService,
+    public alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.getDashboardDetails();
+    this.getOperatorDetails();
+  }
+
+  getOperatorDetails() {
+    let operatorData = JSON.parse(localStorage.getItem('myUser'));
+    this.operatorId = operatorData.id;
   }
 
   getDashboardDetails() {
@@ -45,6 +53,42 @@ export class OperatorDashboardPage implements OnInit {
 
   openProfilePage() {
     this.navCtrl.navigateForward(['/operator-dashboard/operator-profile'])
+  }
+
+  async logoutAlert() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm',
+      message: 'Do you want to logout?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            // console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            this.logout();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  logout() {
+    this.base.deletePlayerID(this.operatorId).subscribe(response => {
+      // alert("Deleted player ID : " + response);
+      if (response) {
+        localStorage.removeItem('myToken');
+        localStorage.removeItem('myUser');
+        this.navCtrl.navigateRoot(['/login']);
+        this.base.toastMessage('Logged out successfully');
+      }
+    });
   }
 
 }
