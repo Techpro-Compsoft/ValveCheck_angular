142a8cd86c3ace8dcc8094a9ea4ec81d1e0859f5
rajaman821 <50168576+amanTP@users.noreply.github.com>
2020-08-21 16:39:46 +0530

    table work bug fixes

--- Frontend/src/app/Operator/operator-dashboard/operator-dashboard.page.ts
+++ Frontend/src/app/Operator/operator-dashboard/operator-dashboard.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { NavController, AlertController } from '@ionic/angular';
 import { OperatorService } from 'src/app/core/Services/Operator/operator.service';
 import { BaseService } from 'src/app/core/Services/base.service';
@@ -13,10 +13,18 @@ export class OperatorDashboardPage implements OnInit {
   blocksArray: Array<object>;
   currentDate: Date;
 
+
+  startIndex: number;
+  itemCount: number;
+  endIndex: number;
+  infinteLoad = true;
+  arr2: Array<object>;
+
   constructor(public navCtrl: NavController,
     public operatorService: OperatorService,
     public base: BaseService,
-    public alertCtrl: AlertController) {
+    public alertCtrl: AlertController,
+    private zone: NgZone) {
     this.currentDate = new Date();
   }
 
@@ -24,40 +32,81 @@ export class OperatorDashboardPage implements OnInit {
   }
 
   ionViewWillEnter() {
+    this.startIndex = 0;
+    this.itemCount = 10;
+    this.endIndex = 10;
     this.getBlocks();
+  }
+
+  loadData(event?) {
+    if (this.endIndex > this.arr2.length) {
+      this.endIndex = this.arr2.length;
+    }
+    if (this.startIndex != this.endIndex) {
+      for (let i = this.startIndex; i < this.endIndex; i++) {
+        if (this.arr2[i]) {
+          this.arr2[i]['expected_stop_time'] = null;
+          if (this.arr2[i]['instruction'] && this.arr2[i]['actual_start_time']) {
+            const d = new Date();
+            let t = this.arr2[i]['actual_start_time'].split(":");
+            let endTime = new Date(d.getFullYear(), d.getMonth(), d.getDate(), parseInt(t[0]), parseInt(t[1]));
+            let instTime = this.arr2[i]['instruction'].split(":")
+            endTime.setHours(endTime.getHours() + parseInt(instTime[0]));
+            endTime.setMinutes(endTime.getMinutes() + parseInt(instTime[1]));
+            this.arr2[i]['expected_stop_time'] = `${endTime.getHours()}:${endTime.getMinutes()}:00`;
+            if (this.arr2[i]['actual_stop_time']) {
+              let actStop = this.arr2[i]['actual_stop_time'].split(":");
+              if (endTime.getHours() === parseInt(actStop[0]) && endTime.getMinutes() === parseInt(actStop[1])) {
+                this.arr2[i]['onTime'] = true;
+              }
+              else {
+                this.arr2[i]['onTime'] = false;
+              }
+            }
+          }
+          this.blocksArray.push(this.arr2[i]);
+        }
+      }
+      this.startIndex = this.endIndex;
+      if (this.endIndex + this.itemCount > this.arr2.length) {
+        this.endIndex = this.endIndex + this.arr2.length - this.endIndex;
+      }
+      else {
+        this.endIndex = this.endIndex + this.itemCount;
+      }
+      if (event) {
+        event.target.complete();
+      }
+    }
+    else {
+      this.infinteLoad = false;
+    }
+  }
+
+  doInfinite(event) {
+    this.loadData(event);
   }
 
   getBlocks() {
     try {
       this.base.getAllBlocks({ role: 3 }).subscribe(response => {
-        if (response.status === "success") {
-          this.blocksArray = response.data;
-          this.blocksArray.forEach(e => {
-            e['expected_stop_time'] = null;
-            if (e['instruction'] && e['actual_start_time']) {
-              const d = new Date();
-              let t = e['actual_start_time'].split(":");
-              let endTime = new Date(d.getFullYear(), d.getMonth(), d.getDate(), parseInt(t[0]), parseInt(t[1]));
-              let instTime = e['instruction'].split(":")
-              endTime.setHours(endTime.getHours() + parseInt(instTime[0]));
-              endTime.setMinutes(endTime.getMinutes() + parseInt(instTime[1]));
-              e['expected_stop_time'] = `${endTime.getHours()}:${endTime.getMinutes()}:00`;
-              if (e['actual_stop_time']) {
-                let actStop = e['actual_stop_time'].split(":");
-                if (endTime.getHours() === parseInt(actStop[0]) && endTime.getMinutes() === parseInt(actStop[1])) {
-                  e['onTime'] = true;
-                }
-                else {
-                  e['onTime'] = false;
-                }
-              }
-            }
+        console.log(response);
+        if (response.status === "success" && response.data.length > 0) {
+          this.infinteLoad = true;
+          this.zone.run(() => {
+            this.arr2 = response.data;
+            this.blocksArray = [];
           });
+          this.loadData();
         }
-      })
+      });
     } catch (error) {
       this.base.toastMessage('Something went wrong');
     }
+  }
+
+  testFn(index: number, item: any): number {
+    return item.id;
   }
 
   viewValves(id, lat, lng) {
